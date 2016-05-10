2a27e6fd42a161fcc49d560e5ae0e41e8fecc144
Pinal Patel <pinal3291@gmail.com>
2016-05-10 13:09:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pinal/maildemo/Config.java
@@ -0,0 +1,10 @@
+package com.example.pinal.maildemo;
+
+/**
+ * Created by pinal on 10/5/16.
+ */
+
+public class Config {
+    public static final String EMAIL ="pinal3291@gmail.com";
+    public static final String PASSWORD ="pinal.credencys";
+}
